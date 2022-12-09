3ddeb48ebe05a2cd7a5873fbb99bd2b5bab07d9f
Daniel Bartolini <djb3149@gmail.com>
2022-12-09 11:11:11 -0500

    added goback functionality, fixed search problems

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 var searchBtn = document.querySelector(".searchBtn");
-var indexSearchBtn = document.querySelector(".indexSearchBtn");
 var userSearch = document.getElementById("user-search");
-var formatEl = document.getElementById("format");
-var q = userSearch.value.trim();
+var formatEl = document.querySelector(".form-select");
 var resultCard = document.querySelector(".result-card");
+var goBackBtn = document.getElementById("goBack");
 
-var displaySearch = function(event) {
-    event.preventDefault();
-    var formatChoice = formatEl.value;
-    if (!document.location.href.includes('search-results')) {
-    document.location.replace('./search-results.html');
-    }
-    
-    console.log(formatChoice);
-
-    if (formatChoice === 'none') {
-        getAllLocData(q.value);
+var displaySearch = function(q, format) {
+    if (format === 'none') {
+        getAllLocData(q);
     } else {
-        getLocData(formatChoice, q.value);
+        getChosenLocData(format, q);
     }
+    console.log(format);
 }
 
-var getLocData = function(format, q) {
+var handleSearch = function(event) {
+    event.preventDefault();
+    var formatChoice = formatEl.value;
+    var q = userSearch.value.trim();
+    if (!document.location.href.includes('search-results')) {
+        var queryParams = "?q=" + q + "&format=" + (formatChoice || "");
+        
+        return document.location.assign('./search-results.html' + queryParams);
+        
+    }   
+    displaySearch(q, formatChoice);
+   
+}
+
+
+
+var getChosenLocData = function(format, q) {
     var formatUrl = 'https://www.loc.gov/' + format + '/?q=' + q + '&fo=json';
     fetch(formatUrl)
     .then(function (response) {
@@ -29,7 +37,6 @@ var getLocData = function(format, q) {
             response.json().then(function (data) {
                 displayResults(data.results, q);
                 console.log(data.results);
-
             });
         } else {
             alert('Error: ' + response.statusText);
@@ -71,33 +78,43 @@ var displayResults = function(items) {
 
         var card = document.createElement("div");
         var cardBody = document.createElement("div");
-        var h3 = document.createElement("h3");
-        var h4 = document.createElement("h4");
+        var h5 = document.createElement("h5");
+        var h6 = document.createElement("h6");
         var p = document.createElement("p");
         var a = document.createElement("a");
 
         card.classList = "card";
         cardBody.classList = "card-body";
-        h3.classList = "card-title";
-        h4.classList = "card-subtitle mb-2 text-muted";
+        h5.classList = "card-title";
+        h6.classList = "card-subtitle mb-2 text-muted";
         p.classList = "card-text";
         a.classList = "card-text";
 
-        h3.textContent = items[i].title;
-        h4.textContent = items[i].group;
+        h5.textContent = items[i].title;
+        h6.textContent = items[i].group;
         p.textContent = items[i].description;
         a.textContent = items[i].url;
         a.href = items[i].url;
 
         resultCard.appendChild(card);
-        resultCard.appendChild(cardBody);
-        resultCard.appendChild(h3);
-        resultCard.appendChild(h4);
-        resultCard.appendChild(p);
-        resultCard.appendChild(a);
-
+        card.appendChild(cardBody);
+        cardBody.appendChild(h5);
+        cardBody.appendChild(h6);
+        cardBody.appendChild(p);
+        cardBody.appendChild(a);
     }
-
 }
-searchBtn.addEventListener('click', displaySearch);
-indexSearchBtn.addEventListener('click', displaySearch);
+
+var goBack = function(event) {
+    event.preventDefault();
+    document.location.assign('./index.html');
+}
+
+searchBtn.addEventListener('click', handleSearch);
+goBackBtn.addEventListener('click', goBack);
+
+if (location.search) {
+    var params = new URLSearchParams(location.search);
+
+    displaySearch(params.get("q"), params.get("format"));
+}
